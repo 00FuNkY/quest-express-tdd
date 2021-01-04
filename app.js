3b2c45ddc484ac3eda608dab0e2e0787740b6c38
Malo Pivert <pivertmalo@gmail.com>
2021-01-04 15:31:03 +0100

    added bookmarks GET and tests for the challenge

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
 // app.js
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+const connection = require("./connection");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+app.post("/bookmarks", (req, res) => {
+  const { url, title } = req.body;
+  if (!url || !title) {
+    res.status(422).json({ error: "required field(s) missing" });
+  } else
+    connection.query(
+      "INSERT INTO bookmark(url, title) VALUES (?,?)",
+      [url, title],
+      (err, results) => {
+        if (err) {
+          res.status(500).json({ error: err.message, sql: err.sql });
+        } else {
+          connection.query(
+            "SELECT * FROM bookmark WHERE id = ?",
+            results.insertId,
+            (err, records) => {
+              if (err) {
+                res.status(500).json({ error: err.message, sql: err.sql });
+              } else {
+                res.status(201).json(records[0]);
+              }
+            }
+          );
+        }
+      }
+    );
+});
+
+app.get("/bookmarks/:id", (req, res) => {
+  const { id } = req.params;
+  connection.query(
+    "SELECT * FROM bookmark WHERE id = ?",
+    [id],
+    (err, results) => {
+      if (err || results.length === 0) {
+        res.status(404).json({ error: "Bookmark not found" });
+      } else {
+        res.status(200).json(results[0]);
+      }
+    }
+  );
+});
 
 module.exports = app;
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,7 +1,10 @@
 const request = require("supertest");
 const app = require("../app");
 
+const connection = require("../connection");
+
 describe("Test routes", () => {
+  beforeEach((done) => connection.query("TRUNCATE bookmark", done));
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get("/")
@@ -12,5 +15,66 @@ describe("Test routes", () => {
         expect(response.body).toEqual(expected);
         done();
       });
+  });
+  it("POST /bookmarks sends json error missing fields", (done) => {
+    request(app)
+      .post("/bookmarks")
+      .send({})
+      .expect(422)
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        const expecting = { error: "required field(s) missing" };
+        expect(response.body).toEqual(expecting);
+        done();
+      });
+  });
+  it("POST /bookmarks sends json success", (done) => {
+    request(app)
+      .post("/bookmarks")
+      .send({ url: "https://jestjs.io", title: "Jest" })
+      .expect(201)
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        const expectings = {
+          id: expect.any(Number),
+          url: "https://jestjs.io",
+          title: "Jest",
+        };
+        expect(response.body).toEqual(expectings);
+        done();
+      });
+  });
+
+  describe("GET /bookmarks/:id", () => {
+    const testBookmark = { url: "https://nodejs.org/", title: "Node.js" };
+    beforeEach((done) =>
+      connection.query("TRUNCATE bookmark", () =>
+        connection.query("INSERT INTO bookmark SET ?", testBookmark, done)
+      )
+    );
+
+    it("GET /bookmarks/:id error when getting a bookmark from his id", (done) => {
+      request(app)
+        .get("/bookmarks/222222")
+        .expect(404)
+        .expect("Content-Type", /json/)
+        .then((response) => {
+          const expecteeed = { error: "Bookmark not found" };
+          expect(response.body).toEqual(expecteeed);
+          done();
+        });
+    });
+
+    it("GET /bookmarks/:id success getting bookmark from his id", (done) => {
+      request(app)
+        .get("/bookmarks/1")
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .then((response) => {
+          const expected = { id: 1, ...testBookmark };
+          expect(response.body).toEqual(expected);
+          done();
+        });
+    });
   });
 });
